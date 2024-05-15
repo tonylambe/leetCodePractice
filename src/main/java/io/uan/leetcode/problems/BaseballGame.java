90e6ec6073cb973ccc73f9cdf5abcb21040f07e1
Tony <alambe@gmail.com>
2024-05-16 00:07:47 +0100

    Baseball game

--- src/main/java/io/uan/leetcode/problems/BaseballGame.java
+++ src/main/java/io/uan/leetcode/problems/BaseballGame.java
@@ -2,11 +2,40 @@ package io.uan.leetcode.problems;
 
 import io.uan.leetcode.common.LeetCodeProblem;
 
+import java.util.ArrayDeque;
+import java.util.Stack;
+
 public class BaseballGame extends LeetCodeProblem {
+
+    ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
 
     public BaseballGame() {
         this.setProblemNumber(682);
         this.setUrl("https://leetcode.com/problems/baseball-game/description/");
+    }
+
+    public int calPoints(String[] operations) {
+        for(String op : operations) {
+            switch(op){
+                case "+":
+                    final int top = stack.pop();
+                    final int secondFromTop = stack.peek();
+                    stack.push(top);
+                    stack.push(top+secondFromTop);
+                    break;
+                case "D":
+                    stack.push(stack.peek()*2);
+                    break;
+                case "C":
+                    stack.pop();
+                    break;
+                default:
+                    stack.push(Integer.parseInt(op));
+            }
+
+        }
+
+        return stack.stream().reduce(0, Integer::sum);
     }
 
 
--- src/test/java/io/uan/leetcode/test/problems/BaseBallGameTest.java
+++ src/test/java/io/uan/leetcode/test/problems/BaseBallGameTest.java
@@ -8,19 +8,23 @@ import io.uan.leetcode.problems.BaseballGame;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BaseBallGameTest extends LeetCodeTest {
 
     private final BaseballGame baseBallGame = new BaseballGame();
 
     @ParameterizedTest
     @MethodSource("stringArrayProvider")
-    public void testBaseBallGame(final String[] input) {
-
+    public void testBaseBallGame(final String[] input, final int actual) {
+        assertEquals(baseBallGame.calPoints(input), actual);
     }
 
     static Stream<Arguments> stringArrayProvider() {
         return Stream.of(
-                Arguments.of((Object) new String[]{"5","2","C","D","+"})
+                Arguments.of((Object) new String[]{"5","2","C","D","+"}, 30),
+                Arguments.of( (Object) new String[]{"5","-2","4","C","D","9","+","+"}, 27),
+                Arguments.of( (Object) new String[]{"1","C"}, 0)
         );
     }
 }
